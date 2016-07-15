73a875cbdaeff4700f7e502e44262047a76a90ff
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-15 11:39:58 -0500

    hexadecimal

---
+++ app.js
@@ -0,0 +1,15 @@
+/****************************************************
+*                     Precision                     *
+****************************************************/
+function myFunction() {
+  var x = 999999999999999;
+  var y = 9999999999999999;
+  document.getElementById("precision").innerHTML = x + "<br>" + y;
+}
+
+/****************************************************
+*                    Hexadecimal                    *
+****************************************************/
+function myHex() {
+  document.getElementById("hex").innerHTML = "0xFF = " + 0xFF;
+}
